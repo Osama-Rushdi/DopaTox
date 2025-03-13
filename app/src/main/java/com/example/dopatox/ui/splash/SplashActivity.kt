3cf3179a64e8576b8037d28c199f8aac1997d780
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2025-03-13 18:03:30 +0200

    create splash-screen with fade-in animation

---
+++ app/src/main/java/com/example/dopatox/ui/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dopatox.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dopatox.R
+import com.example.dopatox.databinding.ActivitySplashBinding
+import com.example.dopatox.ui.home.MainActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val popOutAnimation = Constants.runAnimation(this, R.anim.fade_in)
+        binding.logo.startAnimation(popOutAnimation)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
