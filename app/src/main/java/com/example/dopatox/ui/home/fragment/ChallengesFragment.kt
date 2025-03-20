b2f59eaa45c8129fedcdf5b49f096f8fae857659
Abdallah Elsobky <abdullah442286@fsc.bu.edu.eg>
2025-03-20 19:32:47 +0200

    create home fragments and navigation between them

---
+++ app/src/main/java/com/example/dopatox/ui/home/fragment/ChallengesFragment.kt
@@ -0,0 +1,19 @@
+package com.example.dopatox.ui.home.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dopatox.databinding.FragmentChallengesBinding
+
+class ChallengesFragment : Fragment() {
+    lateinit var binding: FragmentChallengesBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChallengesBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/dopatox/ui/home/fragment/SettingsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.dopatox.ui.home.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dopatox.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    lateinit var binding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
